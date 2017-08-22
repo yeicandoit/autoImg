bdafc68521f5656910b482ad75eb9e0cb5adcdf9
iclick <iclick@iclickdeMacBook-Pro.local>
2017-08-22 14:03:35 +0800

    add date for ad demand

--- webAutoImg/autoimage/views.py
+++ webAutoImg/autoimage/views.py
@@ -4,12 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from autoimage.models import AdDemand
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 def savedemand(request):
     m_post = request.POST
+
     demand = AdDemand(app=m_post['app'], adType=m_post['adType'], adImg=request.FILES.get('adImg'),
                       adCornerImg=request.FILES.get('adCornerImg'), wcType=m_post['wcType'],
-                      network=m_post['network'], time=m_post['time'], battery=float(m_post['battery']))
+                      network=m_post['network'], time=m_post['time'], battery=float(m_post['battery']),
+                      title=m_post['title'], doc=m_post['doc'], date=datetime.date.today().strftime('%Y-%m-%d'))
     demand.save()
     return HttpResponse("successful!!!")

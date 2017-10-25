d6873c32c34172fe9b64ad24a7aded8a8ead72ce
wangqiang <wangqiang@optaim.com>
2017-10-25 16:27:37 +0800

    add transparency util

---
+++ util/p_transparency.py
@@ -0,0 +1,13 @@
+import cv2
+
+if __name__ == '__main__':
+    #corner = cv2.imread("ads/qsbk_ad_corner.png", cv2.IMREAD_UNCHANGED)
+    #transparency = corner[0:56, 126:134]
+    #corner_save = cv2.resize(transparency, (181, 97))
+    #skip = cv2.imread("ads/-1_03.png", cv2.IMREAD_UNCHANGED)
+    #corner_save[32:97, 0:168] = skip
+    #cv2.imwrite("ok.png", corner_save)
+
+    bottom = cv2.imread('bottom.png')
+    bottom[6:26, 43:83] = cv2.imread("../ad_area/shuqi-theme-bottom/battery.png")
+    cv2.imwrite("bottom_new.png", bottom)

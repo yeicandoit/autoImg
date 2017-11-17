a9e55cbbdbc7f955cd60881d9e9ae40ffbeb7bd6
yeicandoit <410342333@qq.com>
2017-11-17 15:39:51 +0800

    Enable to P qnews feeds small and multi

--- autoImg.py
+++ autoImg.py
@@ -1684,10 +1684,10 @@ class QnewsAutoImg(AutoImg):
             'udid': '192.168.56.101:5555',
         }
 
-    def assembleFeedsAd(self):
-        blank_height = self.cf.getint('Qnews', 'blank_height')
-        ad_width = self.cf.getint('Qnews', 'ad_width')
-        ad_height = self.cf.getint('Qnews', 'ad_height')
+    def assembleFeedsBigAd(self):
+        blank_height = self.cf.getint('Qnews', 'feeds_big_blank_height')
+        ad_width = self.cf.getint('Qnews', 'feeds_big_ad_width')
+        ad_height = self.cf.getint('Qnews', 'feeds_big_ad_height')
         word_height = self.cf.getint('Qnews', 'word_height')
         blank = cv2.imread(self.cf.get('Qnews', 'img_blank'))
 
@@ -1717,7 +1717,7 @@ class QnewsAutoImg(AutoImg):
         draw = ImageDraw.Draw(im)
         if '' != self.doc:
             ttfont = ImageFont.truetype("font/UbuntuDroid.ttf", self.cf.getint('Qnews', 'doc_size'))
-            ad_doc_pos = (self.cf.getint('Qnews', 'doc_x'), self.cf.getint('Qnews', 'doc_y'))
+            ad_doc_pos = (self.cf.getint('Qnews', 'feeds_big_doc_x'), self.cf.getint('Qnews', 'feeds_big_doc_y'))
             ad_doc_color = self.parseArrStr(self.cf.get('Qnews', 'doc_color'), ',')
             if len(self.doc) <= doc_1stline_max_len:
                 draw.text(ad_doc_pos, self.doc, fill=(ad_doc_color[0], ad_doc_color[1], ad_doc_color[2]), font=ttfont)
@@ -1731,8 +1731,144 @@ class QnewsAutoImg(AutoImg):
         im.save('tmp_img/tmp.png')
         return cv2.imread('tmp_img/tmp.png')
 
+    def set1stFeedsSmallDocLen(self, doc, sec):
+        cl = self.cf.getint(sec, 'doc_Chinese_width')
+        el = self.cf.getint(sec, 'doc_English_width')
+        fl = self.cf.getint(sec, 'feeds_small_doc_1stline_px_len')
+        mlen = 0
+        for i in range(len(doc)):
+            if doc[i] <= '\u2000':
+                mlen += el
+            # I think Chinese and Chinese punctuation(eg:，。：) consume doc_Chinese_width length px
+            else:
+                mlen += cl
+            if mlen > fl:
+                logger.debug('doc first line len is:%d', i)
+                return i
 
-    def start(self):
+        return len(doc)
+
+    def assembleFeedsSmallAd(self):
+        blank_height = self.cf.getint('Qnews', 'feeds_small_blank_height')
+        ad_width = self.cf.getint('Qnews', 'feeds_small_ad_width')
+        ad_height = self.cf.getint('Qnews', 'feeds_small_ad_height')
+        word_height = self.cf.getint('Qnews', 'word_height')
+        blank = cv2.imread(self.cf.get('Qnews', 'img_blank'))
+        ad_left_x = self.cf.getint('Qnews', 'feeds_small_ad_x')
+        ad_top_y = self.cf.getint('Qnews', 'feeds_small_ad_y')
+
+        doc_1stline_max_len = self.set1stFeedsSmallDocLen(self.doc, 'Qnews')
+        # set ad backgroud
+        bkg = cv2.resize(blank, (self.screen_width, blank_height))
+
+        # Add bottom
+        _, bottom_height = self.getImgWH(self.cf.get('Qnews', 'img_feeds_small_bottom'))
+        bkg[blank_height - bottom_height:blank_height, 0:self.screen_width] \
+            = cv2.imread(self.cf.get('Qnews', 'img_feeds_small_bottom'))
+
+        # Add ad
+        ad = cv2.imread(self.img_paste_ad)
+        ad = cv2.resize(ad, (ad_width, ad_height))
+        bkg[ad_top_y:ad_top_y + ad_height, ad_left_x:ad_left_x + ad_width] = ad
+        cv2.imwrite('tmp_img/tmp.png', bkg)
+
+        # Print doc and desc in the bkg
+        im = Image.open('tmp_img/tmp.png')
+        draw = ImageDraw.Draw(im)
+        if '' != self.doc:
+            ttfont = ImageFont.truetype("font/UbuntuDroid.ttf", self.cf.getint('Qnews', 'doc_size'))
+            ad_doc_pos = (self.cf.getint('Qnews', 'feeds_small_doc_x'), self.cf.getint('Qnews', 'feeds_small_doc_y'))
+            ad_doc_color = self.parseArrStr(self.cf.get('Qnews', 'doc_color'), ',')
+            if len(self.doc) <= doc_1stline_max_len:
+                draw.text(ad_doc_pos, self.doc, fill=(ad_doc_color[0], ad_doc_color[1], ad_doc_color[2]), font=ttfont)
+            else:
+                ad_doc_pos1 = (ad_doc_pos[0], ad_doc_pos[1] + word_height)
+                draw.text(ad_doc_pos, self.doc[:doc_1stline_max_len],
+                          fill=(ad_doc_color[0], ad_doc_color[1], ad_doc_color[2]),
+                          font=ttfont)
+                draw.text(ad_doc_pos1, self.doc[doc_1stline_max_len:],
+                          fill=(ad_doc_color[0], ad_doc_color[1], ad_doc_color[2]),
+                          font=ttfont)
+
+        im.save('tmp_img/tmp.png')
+        return cv2.imread('tmp_img/tmp.png')
+
+    def assembleFeedsMultiAd(self):
+        blank_height = self.cf.getint('Qnews', 'feeds_multi_blank_height')
+        ad_width = self.cf.getint('Qnews', 'feeds_multi_ad_width')
+        one_ad_width = self.cf.getint('Qnews', 'feeds_multi_1_ad_width')
+        ad_height = self.cf.getint('Qnews', 'feeds_multi_ad_height')
+        word_height = self.cf.getint('Qnews', 'word_height')
+        blank = cv2.imread(self.cf.get('Qnews', 'img_blank'))
+
+        doc_1stline_max_len = self.set1stDocLen(self.doc, 'Qnews')
+        # set ad backgroud
+        if len(self.doc) <= doc_1stline_max_len:
+            bkg = cv2.resize(blank, (self.screen_width, blank_height))
+        else:
+            blank_height = blank_height + word_height
+            bkg = cv2.resize(blank, (self.screen_width, blank_height))
+
+        # Add bottom
+        _, bottom_height = self.getImgWH(self.cf.get('Qnews', 'img_ad_area_bottom'))
+        bkg[blank_height - bottom_height:blank_height, 0:self.screen_width] \
+            = cv2.imread(self.cf.get('Qnews', 'img_ad_area_bottom'))
+
+        # Add ad
+        img_ads = self.img_paste_ad.split(',')
+        assert len(img_ads) == 3, "Should have 3 ad images for feeds_multi"
+        ad_top_y = blank_height - bottom_height - ad_height
+        ad_left_x = (self.screen_width - ad_width) / 2
+        ad_space_between = (ad_width - 3*one_ad_width) / 2
+        bkg[ad_top_y:ad_top_y + ad_height, ad_left_x:ad_left_x + one_ad_width] \
+            = cv2.resize(cv2.imread(img_ads[0]), (one_ad_width, ad_height))
+        bkg[ad_top_y:ad_top_y + ad_height, ad_left_x+one_ad_width+ad_space_between:ad_left_x+2*one_ad_width+ad_space_between] \
+            = cv2.resize(cv2.imread(img_ads[1]), (one_ad_width, ad_height))
+        bkg[ad_top_y:ad_top_y+ad_height, ad_left_x+2*one_ad_width+2*ad_space_between:ad_left_x+3*one_ad_width+2*ad_space_between] \
+            = cv2.resize(cv2.imread(img_ads[2]), (one_ad_width, ad_height))
+        cv2.imwrite('tmp_img/tmp.png', bkg)
+
+        # Print doc and desc in the bkg
+        im = Image.open('tmp_img/tmp.png')
+        draw = ImageDraw.Draw(im)
+        if '' != self.doc:
+            ttfont = ImageFont.truetype("font/UbuntuDroid.ttf", self.cf.getint('Qnews', 'doc_size'))
+            ad_doc_pos = (self.cf.getint('Qnews', 'feeds_multi_doc_x'), self.cf.getint('Qnews', 'feeds_multi_doc_y'))
+            ad_doc_color = self.parseArrStr(self.cf.get('Qnews', 'doc_color'), ',')
+            if len(self.doc) <= doc_1stline_max_len:
+                draw.text(ad_doc_pos, self.doc, fill=(ad_doc_color[0], ad_doc_color[1], ad_doc_color[2]), font=ttfont)
+            else:
+                ad_doc_pos1 = (ad_doc_pos[0], ad_doc_pos[1] + word_height)
+                draw.text(ad_doc_pos, self.doc[:doc_1stline_max_len],
+                          fill=(ad_doc_color[0], ad_doc_color[1], ad_doc_color[2]),
+                          font=ttfont)
+                draw.text(ad_doc_pos1, self.doc[doc_1stline_max_len:],
+                          fill=(ad_doc_color[0], ad_doc_color[1], ad_doc_color[2]),
+                          font=ttfont)
+
+        im.save('tmp_img/tmp.png')
+        return cv2.imread('tmp_img/tmp.png')
+
+    def assembleFeedsAd(self):
+        if "feeds_big" == self.ad_type:
+            ad = self.assembleFeedsBigAd()
+        elif "feeds_small" == self.ad_type:
+            ad = self.assembleFeedsSmallAd()
+        elif "feeds_multi" == self.ad_type:
+            ad = self.assembleFeedsMultiAd()
+        else:
+            ad = None
+        return ad
+
+    def getFeedsBlankHeight(self):
+        key = self.ad_type + "_blank_height"
+        blank_height = self.cf.getint('Qnews', key)
+        if len(self.doc) > self.set1stDocLen(self.doc, 'Qnews') and "feeds_small" != self.ad_type:
+            blank_height = blank_height + self.cf.getint('Qnews', 'word_height')
+
+        return blank_height
+
+    def feedsStart(self):
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.desired_caps)
         self.driver.implicitly_wait(10)
         sleep(10)
@@ -1746,15 +1882,15 @@ class QnewsAutoImg(AutoImg):
             except:
                 pass
         sleep(3)
-        blank_height = self.cf.getint('Qnews', 'blank_height')
-        if len(self.doc) > self.set1stDocLen(self.doc, 'Qnews'):
-            blank_height = blank_height + self.cf.getint('Qnews', 'word_height')
+
+        blank_height = self.getFeedsBlankHeight()
         bottom_height = self.cf.getint('Qnews', 'bottom_height')
         # The ad area should be >= the biggest feeds ad height(its doc is two line) and app bottom
         top_left, bottom_right = self.findFeedsArea(self.img_split, self.fp_split, self.img_ad_flag, self.fp_ad_flag,
                                                     blank_height, bottom_height)
         self.driver.get_screenshot_as_file('screenshot.png')
         ad = self.assembleFeedsAd()
+
         img_color = cv2.imread('screenshot.png')
         bottom_y = self.cf.getint('screen', 'height') - bottom_height
         ad_bottom_height = bottom_y - bottom_right[1] - blank_height
@@ -1770,6 +1906,18 @@ class QnewsAutoImg(AutoImg):
         cv2.imwrite(self.composite_ads_path, img_color)
 
         self.driver.quit()
+
+    def bannerStart(self):
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.desired_caps)
+        self.driver.implicitly_wait(10)
+        sleep(10)
+
+        self.driver.quit()
+    def start(self):
+            if 'feeds_banner' != self.ad_type:
+                self.feedsStart()
+            else:
+                self.bannerStart()
 
 class IOSAutoImg(AutoImg):
     def __init__(self, time, battery, img_paste_ad, img_corner_mark='ad_area/corner-mark.png', ad_type='banner',
@@ -1821,9 +1969,12 @@ if __name__ == '__main__':
         #autoImg = ShuQiAutoImg('11:49', 0.8, 'ads/insert-600_500.jpg', 'ad_area/corner-ad.png', 'image_text', '4G')
         #autoImg = IOSAutoImg('11:49', 0.8, 'ads/insert-600_500.jpg', 'ad_area/corner-ad.png', 'image_text', '4G')
         #autoImg = AiqiyiAutoImg('11:49', 0.8, 'ads/insert-600_500.jpg', 'ad_area/corner-ad.png', 'image_text', '4G')
-        autoImg = TianyaAutoImg('11:49', 0.8, 'ads/banner640_100.jpg', 'ad_area/corner-ad.png', 'image_text', '4G')
-        #autoImg = QnewsAutoImg('11:49', 0.8, 'ads/640x246.jpg', 'ad_area/corner-ad.png', 'image_text', '4G',
-        #                       u'吉利新帝豪', u'饼子还能这么吃，秒杀鸡蛋灌饼，完爆煎饼果子，做法还超级简单！')
+        #autoImg = TianyaAutoImg('11:49', 0.8, 'ads/banner640_100.jpg', 'ad_area/corner-ad.png', 'image_text', '4G')
+        autoImg = QnewsAutoImg('11:49', 0.8, 'ads/230x160.jpg,ads/230x160.jpg,ads/230x160.jpg', 'ad_area/corner-ad.png',
+                               'feeds_multi', '4G', u'吉利新帝豪', u'饼子还能这么吃，秒杀鸡蛋灌饼，完爆煎饼果子，做法还超级简单！')
+        #autoImg = QnewsAutoImg('11:49', 0.8, 'ads/230x160.jpg', 'ad_area/corner-ad.png',
+        #                       'feeds_small', '4G', u'吉利新帝豪', u'上海浦东即将举办大型家具展，入场门票免费领')
+
         #autoImg = QzoneAutoImg('16:20', 1, 'ads/feeds1000x560.jpg', 'ads/logo_512x512.jpg', 'image_text',
         #                    'wifi', u'人人车', u'上海卖车车主：测一测你的爱车能卖多少钱！', logo='ads/insert-600_500.jpg')
         autoImg.compositeImage()

8f4fbf329d7199d71d4fc0df77c254799871475c
yeicandoit <410342333@qq.com>
2017-11-21 18:58:40 +0800

    Enable batterydoctor banner

--- ptu/batterydoctor.py
+++ ptu/batterydoctor.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 from appium import webdriver
 from time import sleep
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image
 import cv2
 import imagehash
 import traceback
@@ -23,7 +23,7 @@ class BatteryDoctorAutoImg(Base):
             self.fp_ad_banner_flag = str(imagehash.dhash(Image.fromarray(self.img_ad_banner_flag)))
             self.img_charge = cv2.imread(self.cf.get('BatteryDoctor', 'img_charge'), 0)
             self.fp_charge = str(imagehash.dhash(Image.fromarray(self.img_charge)))
-            self.logger.debug("fp_ad_banner_flag:%s, fp_charge", self.fp_ad_banner_flag, self.fp_charge)
+            self.logger.debug("fp_ad_banner_flag:%s, fp_charge:%s", self.fp_ad_banner_flag, self.fp_charge)
 
         if 'check' == self.ad_type:
             self.img_save = cv2.imread(self.cf.get('BatteryDoctor', 'img_save'), 0)
@@ -68,8 +68,15 @@ class BatteryDoctorAutoImg(Base):
         ok, tl, br = self.findElementPos4Awhile(self.img_charge, self.fp_charge)
         assert ok, "Do not find charge element in batterydoctor"
         self.clickEliment(self.device_udid, tl[0], tl[1])
-        assert self.findElement4Awhile(self.img_ad_banner_flag, self.fp_ad_banner_flag), "Do not find banner in batterydoctor"
-
+        assert self.findElement4Awhile(self.img_ad_banner_flag,
+                                       self.fp_ad_banner_flag), 'Do not find banner in batterydoctor'
+        ad_origin = cv2.imread(self.img_paste_ad)
+        ad_size = self.parseArrStr(self.cf.get('BatteryDoctor', 'ad_banner_size'), ',')
+        ad = cv2.resize(ad_origin, (ad_size[0], ad_size[1]))
+        ad_pos = self.parseArrStr(self.cf.get('BatteryDoctor', 'ad_banner_pos'), ',')
+        img_color = cv2.imread('screenshot.png')
+        img_color[ad_pos[1]:ad_pos[1] + ad_size[1], ad_pos[0]:ad_pos[0] + ad_size[0]] = ad
+        cv2.imwrite(self.composite_ads_path, self.setHeader(img_color))
 
         self.driver.quit()
 
@@ -97,7 +104,7 @@ class BatteryDoctorAutoImg(Base):
 if __name__ == '__main__':
     try:
         autoImg = BatteryDoctorAutoImg('11:49', 0.8, 'ads/640x330.jpg', '../ad_area/corner-ad.png',
-                               'check', '4G', u'吉利新帝豪', u'饼子还能这么吃，秒杀鸡蛋灌饼，完爆煎饼果子，做法还超级简单！')
+                               'banner', '4G', u'吉利新帝豪', u'饼子还能这么吃，秒杀鸡蛋灌饼，完爆煎饼果子，做法还超级简单！')
         autoImg.compositeImage()
     except Exception as e:
         traceback.print_exc()
